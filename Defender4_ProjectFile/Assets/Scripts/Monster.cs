725c6195ed3cbf1c23f90b81a92b20ad719dc940
skm00147 <103888310+skm00147@users.noreply.github.com>
2023-07-13 07:08:31 +0900

    몹 기능명세서 작성

---
+++ Defender4_ProjectFile/Assets/Scripts/Monster.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Monster : MonoBehaviour
+{
+    RaycastHit2D ScanObject;
+    Rigidbody2D rigid;
+    public float AttackRange, WalkSpeed;
+    public int HP, Atk;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScanObject = Physics2D.Raycast(rigid.position, Vector2.left, AttackRange);
+        if(ScanObject.transform.CompareTag("Barrier"))
+        {
+            Attack();
+        }
+        else
+        {
+            rigid.velocity = Vector2.left * WalkSpeed;
+        }
+    }
+
+    public abstract void Attack();
+}
