44c1de8f3d2a065627f247bb7dcd5e98937e9891
gimgangsan <gimgansan@gmail.com>
2023-07-15 15:08:26 +0900

    맵 제작

---
+++ Defender4_ProjectFile/Assets/Scripts/Castle.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Castle : HealthObject
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(100);
+        }
+    }
+
+    public override void Die()
+    {
+        Debug.Log("¼º »Ñ¼ÅÁü. °ÔÀÓ¿À¹ö Ã¢ ¶ç¿ì±â°¡ ÇÊ¿äÇÔ");
+        base.Die();
+    }
+}
