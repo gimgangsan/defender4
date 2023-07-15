25d0726ad86884e73469d86b2b5fccaf05d322c1
gimgangsan <gimgansan@gmail.com>
2023-07-15 14:37:36 +0900

    체력 바 있는 오브젝트 구현

---
+++ Defender4_ProjectFile/Assets/Scripts/HealthObject.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthObject : MonoBehaviour
+{
+    [SerializeField]
+    private Slider HealthBar;
+
+    public float MaxHP;
+    protected float CurrentHP;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        CurrentHP = MaxHP;
+        HealthBar.maxValue = MaxHP;
+        HealthBar.value = CurrentHP;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        CurrentHP -= damage;
+        HandleHpBar();
+
+        if(CurrentHP < 0)
+        {
+            Die();
+        }
+    }
+
+    private void HandleHpBar()
+    {
+        HealthBar.value = CurrentHP;
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
